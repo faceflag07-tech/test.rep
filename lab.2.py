59021df839aa7ec373332619987086887f8d3db5
Yodanas <faceflag07@gmail.com>
2025-11-23 20:03:40 +0200

    п

--- lab.2.py
+++ lab.2.py
@@ -1,5 +1,8 @@
-n = int(input("Enter the number : "))  
+n = int(input("Введіть n: "))
 
-    
-for i in range(1, n+1):
-    print(i, end=" ")
+for i in range(1, n + 1):
+    divisors = 0
+    for j in range(1, i + 1):
+        if i % j == 0:
+            divisors += 1
+    print(str(i) + "+" * divisors)
